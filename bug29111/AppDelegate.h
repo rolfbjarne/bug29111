b924e68183b45ce58becc95993dea884017a2e5c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2015-04-15 11:41:53 +0200

    Initial Commit

---
+++ bug29111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bug29111
+//
+//  Created by Rolf Bjarne Kvinge on 15/04/15.
+//  Copyright (c) 2015 Rolf Bjarne Kvinge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bug29111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bug29111
+//
+//  Created by Rolf Bjarne Kvinge on 15/04/15.
+//  Copyright (c) 2015 Rolf Bjarne Kvinge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
